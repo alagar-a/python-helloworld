4e9b9dff1915dfaa2acf6332cb2aee367efc8bfb
Alagarsamy Arunachalam <aarunach@aarunach-ld1.linkedin.biz>
2018-02-21 17:24:13 +0530

    update

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import sys
 import helloworld.main
+import os
 
 if __name__ == '__main__':
+    print "hello1"
+    print os.environ["AW_PASSWORD"]
     sys.exit(helloworld.main.main())
