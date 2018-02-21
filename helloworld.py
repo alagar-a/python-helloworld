1dd6a60e85649d5a4bd9551a2202e3d3cf1b1234
Alagarsamy Arunachalam <aarunach@aarunach-ld1.linkedin.biz>
2018-02-21 19:19:40 +0530

    dsf

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 import sys
-import helloworld.main
 import os
 
 if __name__ == '__main__':
     print "hello1"
-    variables = os.environ
-    for k in variables.keys():
-      print k
-      print variables[k]
-    sys.exit(helloworld.main.main())
+    print sys.argv[1]
